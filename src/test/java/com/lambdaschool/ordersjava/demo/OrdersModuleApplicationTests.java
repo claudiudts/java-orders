b38dcb5b5206b6957864074fdbe7b5a44849f810
Claudiuts <sarb.claudiu391@gmail.com>
2021-03-15 18:46:40 -0500

    started work

---
+++ src/test/java/com/lambdaschool/ordersjava/demo/OrdersModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.ordersjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
